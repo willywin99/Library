8f4938f4039aa237263cf7749b84e86bc4d5256b
willywin99 <willywinsianturi@gmail.com>
2021-01-27 21:32:04 +0700

    make model and migration for libraries

---
+++ app/Http/Controllers/LibraryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Library;
+use Illuminate\Http\Request;
+
+class LibraryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Library $library)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Library $library)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Library  $library
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Library $library)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_01_27_141325_create_libraries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libraries', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->integer('jumlah_halaman');
+            $table->date('tanggal_terbit');
+            $table->string('ISBN');
+            $table->string('bahasa');
+            $table->string('penerbit');
+            $table->string('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libraries');
+    }
+}
